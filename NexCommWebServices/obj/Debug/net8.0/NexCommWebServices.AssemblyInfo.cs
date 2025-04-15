a2fe8aa71ad946cd181b90c12c05537d374a5e9a
kavin <kavinkumark554@gmail.com>
2025-04-15 17:20:38 +0530

    demo

--- NexCommWebServices/obj/Debug/net8.0/NexCommWebServices.AssemblyInfo.cs
+++ NexCommWebServices/obj/Debug/net8.0/NexCommWebServices.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NexCommWebServices")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e94f84bfbf489195e513f1af7e7769d9596e658f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9895a1fc881a453790d056aa014fff0fa4f27ab1")]
 [assembly: System.Reflection.AssemblyProductAttribute("NexCommWebServices")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NexCommWebServices")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
